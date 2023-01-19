6f7efa778e397767df9ca59af7d27cbaacfba63d
vinhphan812 <vinhphan812@gmail.com>
2023-01-19 17:39:57 +0700

    sync

--- configs/index.js
+++ configs/index.js
@@ -2,16 +2,21 @@ const mongoose = require("mongoose");
 
 const { db_url, db_user, db_pass, db_name } = process.env;
 
-
 module.exports = {
-    initDatabase: () => {
-        // connect database from .env
-        mongoose.set("strictQuery", true);
-        if (db_url)
-            mongoose.connect(db_url, {
-                user: db_user,
-                pass: db_pass,
-                dbName: db_name,
-            });
-    },
+	initDatabase: () => {
+		// connect database from .env
+		mongoose.set("strictQuery", true);
+		if (db_url)
+			mongoose.connect(
+				db_url,
+				{
+					user: db_user,
+					pass: db_pass,
+					dbName: db_name,
+				},
+				(err) => {
+					console.log(err);
+				}
+			);
+	},
 };
