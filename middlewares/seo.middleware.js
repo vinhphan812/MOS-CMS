89b08ec241a9d8f2c68405bd405f7a831b30f3ed
vinhphan812 <vinhphan812@gmail.com>
2023-02-05 15:26:36 +0700

    sync

--- middlewares/seo.middleware.js
+++ middlewares/seo.middleware.js
@@ -37,7 +37,7 @@ module.exports = {
             description: "",
             keywords: [],
             url: "https://" + req.hostname + req.url,
-            image: "https://" + req.hostname + "/public/favicon.png",
+            image: "https://" + req.hostname + "/public/favicon.ico",
         };
 
         // saving path for check
